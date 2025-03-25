1b6047adb9cfbcbb074242b845f6541f1c16b9e1
Signar Kamparås <kamparassignar@gmail.com>
2025-03-25 22:15:34 +0100

    Add PushNotificationService.kt

---
+++ server/src/main/kotlin/services/PushNotificationService.kt
@@ -0,0 +1,13 @@
+package services
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+class PushNotificationService {
+
+}
