64280120aabf4091a695d8fb9b5c8f9ee8d94c04
Signar Kamparås <kamparassignar@gmail.com>
2025-03-26 00:58:38 +0100

    Update Application.kt

--- server/src/main/kotlin/org/erbeenjoyers/drinkwater/Application.kt
+++ server/src/main/kotlin/org/erbeenjoyers/drinkwater/Application.kt
@@ -5,6 +5,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.websocket.WebSockets
+import routes.pushRoutes
+import routes.webSocketRoutes
 
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
@@ -12,9 +15,10 @@ fun main() {
 }
 
 fun Application.module() {
+    install(WebSockets)
+
     routing {
-        get("/") {
-            call.respondText("Ktor: ${Greeting().greet()}")
-        }
+        webSocketRoutes()
+        pushRoutes()
     }
 }
