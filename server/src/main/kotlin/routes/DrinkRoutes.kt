a4edbb674b66802a2c2557786730bbee351fe74e
Signar Kamparås <kamparassignar@gmail.com>
2025-03-26 01:16:05 +0100

    Add DrinkRoutes.kt

---
+++ server/src/main/kotlin/routes/DrinkRoutes.kt
@@ -0,0 +1,34 @@
+package routes
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import database.DrinkLogs
+import database.Users
+import database.Drinks
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun Route.drinkRoutes() {
+    post("/drink") {
+        val request = call.receive<DrinkLogRequest>()
+        transaction {
+            DrinkLogs.insert {
+                it[userId] = request.userId
+                it[drinkId] = request.drinkId
+                it[timestamp] = System.currentTimeMillis()
+            }
+        }
+        call.respondText("Dricklogg sparad!")
+    }
+
+    get("/drinks") {
+        val drinks = transaction {
+            Drinks.selectAll().map { it[Drinks.name] }
+        }
+        call.respond(drinks)
+    }
+}
+
+data class DrinkLogRequest(val userId: Int, val drinkId: Int)
