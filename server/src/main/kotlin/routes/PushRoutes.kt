1c597fd0308ae7a111804e8e8533ba07d181b6c6
Signar Kamparås <kamparassignar@gmail.com>
2025-03-26 01:00:47 +0100

    Add PushRoutes.kt

---
+++ server/src/main/kotlin/routes/PushRoutes.kt
@@ -0,0 +1,25 @@
+package routes
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import services.PushNotificationService
+
+fun Route.pushRoutes() {
+    webSocket("/push/{userId}") {
+        val userId = call.parameters["userId"] ?: return@webSocket close()
+        PushNotificationService.registerClient(userId, this)
+
+        try {
+            for (frame in incoming) {
+                if (frame is Frame.Text) {
+                    val message = frame.readText()
+                    println("Mottog push-meddelande: $message")
+                }
+            }
+        } finally {
+            close()
+        }
+    }
+}
