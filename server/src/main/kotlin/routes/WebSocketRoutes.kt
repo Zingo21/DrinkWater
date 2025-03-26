ba7a4b17fcfbecd8ff5c8708c36a59a644d2c3c1
Signar Kamparås <kamparassignar@gmail.com>
2025-03-26 01:01:04 +0100

    Add WebSocketRoutes.kt

---
+++ server/src/main/kotlin/routes/WebSocketRoutes.kt
@@ -0,0 +1,28 @@
+package routes
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.util.concurrent.ConcurrentHashMap
+
+val connections = ConcurrentHashMap.newKeySet<DefaultWebSocketServerSession>()
+
+fun Route.webSocketRoutes() {
+    webSocket("/drinkUpdates") {
+        connections.add(this)
+        try {
+            for (frame in incoming) {
+                if (frame is Frame.Text) {
+                    val message = frame.readText()
+                    // Skicka meddelandet till alla anslutna klienter
+                    connections.forEach { session ->
+                        session.send("En vän har druckit: $message")
+                    }
+                }
+            }
+        } finally {
+            connections.remove(this)
+        }
+    }
+}
